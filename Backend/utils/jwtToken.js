401828fc77ea74b7cf8228bdbe2b896feac73138
Ratikanta Behera <ratikantabehera360@gmail.com>
2024-12-15 20:57:53 +0530

    Logout fixed

--- Backend/utils/jwtToken.js
+++ Backend/utils/jwtToken.js
@@ -1,17 +1,19 @@
 export const generateToken = (user, message, statusCode, res) => {
-    const token = user.generateJsonWebToken();
-    res
-      .status(statusCode)
-      .cookie("token", token, {
-        expires: new Date(
-          Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
-        ),
-        httpOnly: true,
-      })
-      .json({
-        success: true,
-        message,
-        user,
-        token,
-      });
-  };
+  const token = user.generateJsonWebToken();
+  res
+    .status(statusCode)
+    .cookie("token", token, {
+      expires: new Date(
+        Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+      ),
+      httpOnly: true,
+      secure: true, // Ensures the cookie is sent only over HTTPS
+      sameSite: "None", // Allows cross-site cookies
+    })
+    .json({
+      success: true,
+      message,
+      user,
+      token,
+    });
+};
